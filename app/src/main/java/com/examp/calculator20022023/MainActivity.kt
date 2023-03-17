b55b47f42924fec77bd7879c88f0f382b931eac6
Phat <phatdroid94@gmail.com>
2023-03-17 21:08:42 +0700

    anh xa

--- app/src/main/java/com/examp/calculator20022023/MainActivity.kt
+++ app/src/main/java/com/examp/calculator20022023/MainActivity.kt
@@ -2,10 +2,31 @@ package com.examp.calculator20022023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edtNumber1: EditText
+    private lateinit var edtNumber2: EditText
+    private lateinit var textViewResult: TextView
+    private lateinit var btnPlus: Button
+    private lateinit var btnMinus: Button
+    private lateinit var btnMultiplication: Button
+    private lateinit var btnDivision: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtNumber1 = findViewById(R.id.edit_text_number1)
+        edtNumber2 = findViewById(R.id.edit_text_number2)
+        textViewResult = findViewById(R.id.text_view_result)
+        btnPlus = findViewById(R.id.button_plus)
+        btnMinus = findViewById(R.id.button_minus)
+        btnMultiplication = findViewById(R.id.button_multiplication)
+        btnDivision = findViewById(R.id.button_division)
     }
 }
