68bf2a573345bd4bafd53de50c07708430294ef8
Phat <phatdroid94@gmail.com>
2023-03-17 21:39:41 +0700

    phep cong

--- app/src/main/java/com/examp/calculator20022023/MainActivity.kt
+++ app/src/main/java/com/examp/calculator20022023/MainActivity.kt
@@ -3,9 +3,12 @@ package com.examp.calculator20022023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,5 +31,25 @@ class MainActivity : AppCompatActivity() {
         btnMinus = findViewById(R.id.button_minus)
         btnMultiplication = findViewById(R.id.button_multiplication)
         btnDivision = findViewById(R.id.button_division)
+
+        btnPlus.setOnClickListener(object : OnClickListener{
+            override fun onClick(p0: View?) {
+                val textNumber1 = edtNumber1.text.toString()
+                val textNumber2 = edtNumber2.text.toString()
+
+                // Kiểm tra coi có giá trị hay không
+                if (textNumber1.isEmpty() || textNumber2.isEmpty()) {
+                    Toast.makeText(this@MainActivity, "Bạn chưa nhập đầy đủ!!!", Toast.LENGTH_SHORT).show()
+                    return
+                }
+
+                val number1 = textNumber1.toInt()
+                val number2 = textNumber2.toInt()
+
+                val result = number1 + number2
+
+                textViewResult.text = "Result: $number1 + $number2 = $result"
+            }
+        })
     }
 }
